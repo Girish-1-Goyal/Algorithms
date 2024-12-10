152940fd39a48de22fd392185565a8ed90f367a8
Girish Kumar Goyal <lucifersdecp@gmail.com>
2024-12-10 15:04:15 +0530

    Create Binary.cpp
    Binary To Decimal

---
+++ Binary.cpp
@@ -0,0 +1,62 @@
+/*
+*
+*    Author : Girish Kumar Goyal.
+*
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < class c > struct rge {
+    c b, e;
+};
+template < class c > rge<c> range(c i, c j){
+    return rge<c>{i, j};
+}
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+    ~debug() { cerr << endl; }
+    template < class c > typename enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+        cerr << boolalpha << i;
+        return * this;
+    }
+    template < class c > typename enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+        return * this << range(begin(i), end(i)); 
+    }
+    template < class c, class b > debug & operator << (pair < b, c > d) {
+        return * this << "(" << d.first << ", " << d.second << ")";
+    }
+    template < class c > debug & operator <<(rge<c> d) {
+        *this << "[";
+        for (auto it = d.b; it != d.e; ++it)
+            *this << ", " + 2 * (it == d.b) << *it;
+        return * this << "]";
+    }
+};  
+#define imie(...) " [" << #__VA_ARGS__ " : " << (__VA_ARGS__) << "]"
+
+int binary_decimal(int n){
+	int num = n;
+	int base = 1;
+	int temp = num;
+	int cnt = 0;
+	while(temp){
+		int last = temp % 10;
+		temp /= 10;
+		cnt += last * base;
+		base *= 2;
+	}
+	return cnt;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << binary_decimal(n) << "\n";
+    return 0;
+}
